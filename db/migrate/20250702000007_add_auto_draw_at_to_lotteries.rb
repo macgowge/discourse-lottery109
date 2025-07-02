9d29255a55782e79a8429e2e053343b2c1c85ea3
macgowge <59905570+macgowge@users.noreply.github.com>
2025-07-03 00:03:56 +0800

    Update 20250702000007_add_auto_draw_at_to_lotteries.rb

--- db/migrate/20250702000007_add_auto_draw_at_to_lotteries.rb
+++ db/migrate/20250702000007_add_auto_draw_at_to_lotteries.rb
@@ -1,6 +1,6 @@
 # plugins/discourse-lottery/db/migrate/20250702000007_add_auto_draw_at_to_lotteries.rb
 class AddAutoDrawAtToLotteries < ActiveRecord::Migration[7.0]
   def change
-    add_column :lotteries, :auto_draw_at, :datetime
+    add_column :lottery_plugin_lotteries, :auto_draw_at, :datetime
   end
 end
