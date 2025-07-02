fbded04b6fd100918c8b2af0042e8fae287260a0
macgowge <59905570+macgowge@users.noreply.github.com>
2025-07-02 23:35:01 +0800

    Create 20250702000005_add_drawn_at_to_lotteries.rb

---
+++ db/migrate/20250702000005_add_drawn_at_to_lotteries.rb
@@ -0,0 +1,6 @@
+# plugins/discourse-lottery/db/migrate/202507020001_add_drawn_at_to_lotteries.rb
+class AddDrawnAtToLotteries < ActiveRecord::Migration[7.0]
+  def change
+    add_column :lotteries, :drawn_at, :datetime
+  end
+end
